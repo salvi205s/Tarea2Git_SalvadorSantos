3b540a6908f3db07fd5e03cebe51efd8c8e3b503
salvi205 <salvi205s@gmail.com>
2019-11-29 11:02:23 +0100

    Creación del proyecto

---
+++ src/tarea2git_salvadorsantos/Tarea2Git_SalvadorSantos.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea2git_salvadorsantos;
+
+/**
+ *
+ * @author Salva
+ */
+public class Tarea2Git_SalvadorSantos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
